82a6bf6f4ea4ca68c14e63d2c9c568506ab0ef08
pstacks <pstacks1031@gmail.gov>
2020-11-19 19:07:48 -0600

    User input phase

--- deathroll.py
+++ deathroll.py
@@ -2,34 +2,65 @@
 
 
 import random
-#6 sided
 
+#4
+def roll_4():
+    roll = random.randit(1,4)
+    print(roll)
 
+# 6
 def roll_6():
     roll = random.randint(1,6)
     print(roll)
 
-# roll_6()
+# 8
+def roll_8():
+    roll = random.randit(1,8)
+    print(roll)
+
+# 10
+def roll_10():
+    roll = random.randit(1,10)
+    print(roll)
+
+# 10 (00-90)
+def roll_10_0():
+    roll = random.randit(00,90)
+    print(roll)
+
+# 12
+def roll_12():
+    roll = random.randit(0,12)
+    print(roll)
+
 
 # 20
 def roll_20():
     roll = random.randint(1,20)
     print(roll)
 
+def output():
+    dnum = print(input("Roll?: "))
+    if dnum in ("4"):
+        roll_4
+    
+
+output()
 
 
 
-# 8
-def roll_8():
-    roll = random.randint(1,8)
-    print(roll)
 
-# roll_20()
 
-while (input("Roll: ") in (6)
 
-if input("Roll: ") in (8)
-    roll_8()
 
-if input("Roll: ") in (20):
-    roll_20()
+
+
+
+
+# while (input("Roll: ") in (6)
+
+# if input("Roll: ") in (8)
+#     roll_8()
+
+# if input("Roll: ") in (20):
+#     roll_20()
