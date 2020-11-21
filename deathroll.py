351e05daf9d127aaacfcec1549008557eb3e8cdb
pstacks <pstacks1031@gmail.gov>
2020-11-20 23:29:20 -0600

    Finally got this thing working

--- deathroll.py
+++ deathroll.py
@@ -3,9 +3,15 @@
 
 import random
 
+#2
+def coin_flip():
+    flip = random.randint(1,2)
+    print(flip)
+
+
 #4
 def roll_4():
-    roll = random.randit(1,4)
+    roll = random.randint(1,4)
     print(roll)
 
 # 6
@@ -15,22 +21,22 @@ def roll_6():
 
 # 8
 def roll_8():
-    roll = random.randit(1,8)
+    roll = random.randint(1,8)
     print(roll)
 
 # 10
 def roll_10():
-    roll = random.randit(1,10)
+    roll = random.randint(1,10)
     print(roll)
 
 # 10 (00-90)
-def roll_10_0():
-    roll = random.randit(00,90)
+def roll_00():
+    roll = random.randint(00,90)
     print(roll)
 
 # 12
 def roll_12():
-    roll = random.randit(0,12)
+    roll = random.randint(0,12)
     print(roll)
 
 
@@ -39,28 +45,25 @@ def roll_20():
     roll = random.randint(1,20)
     print(roll)
 
-def output():
-    dnum = print(input("Roll?: "))
-    if dnum in ("4"):
-        roll_4
-    
 
+
+# output
+def output():
+    dnum = input("Roll:? ")
+    if dnum in ("4"):
+        print(roll_4())
+    if dnum in ("6"):
+        print(roll_6())
+    if dnum in ("8"):
+        print(roll_8())
+    if dnum in ("10"):
+        print(roll_10())
+    if dnum in ("00"):
+        print(roll_00())
+    if dnum in ("12"):
+        print(roll_12())
+    if dnum in ("20"):
+        print(roll_20())
+            
 output()
 
-
-
-
-
-
-
-
-
-
-
-# while (input("Roll: ") in (6)
-
-# if input("Roll: ") in (8)
-#     roll_8()
-
-# if input("Roll: ") in (20):
-#     roll_20()
